4e926b9063da44724d1477b3de03e7f8d37ba1d8
kel <akyl_0221@mail.ru>
2019-08-16 09:32:29 +0600

    refactor delete set

--- online_converter/online_converter/urls.py
+++ online_converter/online_converter/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from youtube_converter import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('history/', views.history),
     path('', views.converter, name='converter'),
 ]
